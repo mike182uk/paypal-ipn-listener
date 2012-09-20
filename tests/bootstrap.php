f8abe9379d7045d4b946d94f278b3d28d314d817
Mike Barrett <mike182uk@gmail.com>
2012-09-20 21:33:45 +0100

    add phpunit bootstrap file to use for unit testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require './src/PayPal/Ipn/Response.php';
+require './src/PayPal/Ipn/Response/Standard.php';
+require './src/PayPal/Ipn/Request.php';
+require './src/PayPal/Ipn/Request/cURL.php';
+require './src/PayPal/Ipn/Request/Socket.php';
+require './src/PayPal/Ipn/Listener.php';
