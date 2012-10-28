c8d615c7a8e0b41842825d25edcc1367479b340b
Mike Barrett <mike182uk@gmail.com>
2012-10-28 17:10:18 +0000

    added extra tests

--- tests/CurlRequestTest.php
+++ tests/CurlRequestTest.php
@@ -15,4 +15,28 @@ class CurlRequestTest extends PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf('\PayPal\Ipn\Request', $this->curlRequestObj);
     }
+
+    public function testExceptionIsThrownOnCurlError()
+    {
+    	$this->setExpectedException('\PayPal\Ipn\Exception\CurlRequestException');
+
+    	$mockCurlRequest = new MockCurlRequest();
+    	
+    	$mockCurlRequest->send();
+    }
+
+    public function testSend()
+    {
+    	$this->curlRequestObj->send();
+
+    	$this->assertEquals(200, $this->curlRequestObj->getResponse()->getStatus());
+    }
+}
+
+class MockCurlRequest extends \PayPal\Ipn\Request\cURL
+{
+    public function getRequestUri()
+    {
+    	return '';
+    }
 }
