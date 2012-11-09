63609040b524c156d15ffcf274aca02447ac85ff
Mike Barrett <mike182uk@gmail.com>
2012-11-09 22:35:58 +0000

    update Request\Curl file reference

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,6 @@ require './src/PayPal/Ipn/Exception/UnexpectedResponseStatusException.php';
 require './src/PayPal/Ipn/Response.php';
 require './src/PayPal/Ipn/Response/Standard.php';
 require './src/PayPal/Ipn/Request.php';
-require './src/PayPal/Ipn/Request/cURL.php';
+require './src/PayPal/Ipn/Request/Curl.php';
 require './src/PayPal/Ipn/Request/Socket.php';
 require './src/PayPal/Ipn/Listener.php';
